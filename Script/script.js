76353d71d5b011fc516df6a17451285702f7c5fd
Cabral <marcelo.cabralc2@gmail.com>
2021-08-27 23:26:17 -0300

    First Version
    Primeira versão feita pelo Vinicius.
    
    Co-Authored-By: VinnieJester <89673257+VinnieJester@users.noreply.github.com>

---
+++ Script/script.js
@@ -0,0 +1,48 @@
+window.onload = initPage;
+
+function initPage() {
+  console.log("Fabrício trabalha na Nuuvem");
+  var results = 0;
+  var input = document.getElementById("input");
+  console.log(input)
+  var red = document.getElementById("RED");
+  red.onclick = function() {
+    if (input.value >= 10) {
+      return
+    }
+    results = results + 1
+    input.value = results
+    console.log(results)
+  }
+  console.log(red)
+
+  var green = document.getElementById("GREEN");
+  green.onclick = function() {
+    if (input.value <= 0) {
+      return
+    }
+    results = results - 1
+    input.value = results
+    console.log(input.value)
+    console.log(results)
+  }
+  console.log(green)
+
+  var blue = document.getElementById("BLUE");
+  blue.onclick = function() {
+    if (input.value >= 10) {
+      return
+    }
+    if (input.value == 9) {
+      results = 10;
+      input.value = results
+      return
+    }
+    results = results + 2
+    input.value = results
+    console.log(results)
+  }
+  console.log(blue)
+
+
+}
